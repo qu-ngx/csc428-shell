8653e01fa736096d4fe5e2fbfa6b2032be9bcb04
Quang D. Nguyen <quang13012005@gmail.com>
2025-10-13 21:06:51 -0400

    added lib

---
+++ shell.c
@@ -0,0 +1,12 @@
+#define _POSIX_C_SOURCE 200809L
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 256
+#define PATH_SEP ':'
+
